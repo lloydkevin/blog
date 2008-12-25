ad1ec7aa66ebbef42df8fd2778b87c144d836d8b
Kevin Lloyd <bazil749@gmail.com>
2008-12-25 00:57:38 -0600

    Modify  Head For Lua

--- lua/header.php
+++ lua/header.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head profile="http://gmpg.org/xfn/11">
+<head>
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-
 <title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
+<style type="text/css" media="screen">
+<!-- @import url( <?php bloginfo('stylesheet_url'); ?>?v1.23 ); -->
+</style>
 
 <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
 <link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
 <link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 <?php if ( is_single() ) wp_enqueue_script( 'comment-reply' ); ?>
-<?php //wp_get_archives('type=monthly&format=link'); ?>
+<!-- Wordpress Header -->
 <?php wp_head(); ?>
-<style type="text/css" media="screen">
-<!-- @import url( <?php bloginfo('stylesheet_url'); ?>?v1.23 ); -->
-</style>
+<!-- Wordpress Header -->
 <meta name="verify-v1" content="xzkTbJfyPi4Nf8labnG5uHr5HacZmUQBr4/q+SYu+vQ=" />
 <meta name="y_key" content="27e79c5b6a459004" />
 </head>
